f6ab6643887767e5486897f25db434f4b22ba8d1
goblin <goblin@local>
2025-12-18 23:44:48 +0800

    style: format dto code

--- internal/dto/user.go
+++ internal/dto/user.go
@@ -1,44 +1,49 @@
 package dto
 
-type LoginReq struct {
-	Account   string `json:"account"`
-	Password  string `json:"password"`
-	Email     string `json:"email"`
-	Code      string `json:"code"`
-	LoginType string `json:"login_type"`
-}
+type (
+	LoginReq struct {
+		Account   string `json:"account"`
+		Password  string `json:"password"`
+		Email     string `json:"email"`
+		Code      string `json:"code"`
+		LoginType string `json:"login_type"`
+	}
+	LoginResp struct {
+		Token    string `json:"token"`
+		Username string `json:"username"`
+		Avatar   string `json:"avatar"`
+	}
+)
 
-type LoginResp struct {
-	Token    string `json:"token"`
-	Username string `json:"username"`
-	Avatar   string `json:"avatar"`
-}
+type (
+	GetCodeReq struct {
+		Email string `json:"email"`
+	}
+	GetCodeResp struct {
+		Code string `json:"code"`
+	}
+)
 
-type GetCodeReq struct {
-	Email string `json:"email"`
-}
-type GetCodeResp struct {
-	Code string `json:"code"`
-}
+type (
+	RegisterReq struct {
+		Account  string `json:"account"`
+		Password string `json:"password"`
+		Username string `json:"username"`
+		Email    string `json:"email"`
+		Code     string `json:"code"`
+	}
+	RegisterResp struct {
+		Message string `json:"message"`
+	}
+)
 
-type RegisterReq struct {
-	Account  string `json:"account"`
-	Password string `json:"password"`
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Code     string `json:"code"`
-}
-
-type RegisterResp struct {
-	Message string `json:"message"`
-}
-
-type ResetPasswordReq struct {
-	Email       string `json:"email"`
-	Code        string `json:"code"`
-	NewPassword string `json:"new_password"`
-}
-
-type ResetPasswordResp struct {
-	Message string `json:"message"`
-}
+type (
+	ResetPasswordReq struct {
+		Email       string `json:"email"`
+		Code        string `json:"code"`
+		NewPassword string `json:"new_password"`
+	}
+	ResetPasswordResp struct {
+		Message string `json:"message"`
+	}
+)
