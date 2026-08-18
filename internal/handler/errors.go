9ef936f599ee3b95adadd953f4324ff971e87b2c
chungui <2896151192@qq.com>
2026-08-18 20:09:35 +0800

    refactor: remove stale handler errors

--- internal/handler/errors.go
+++ internal/handler/errors.go
@@ -3,10 +3,7 @@ package handler
 import "errors"
 
 var (
-	ErrChatStream       = errors.New("对话流失败")
 	ErrFileRead         = errors.New("文件读取失败")
 	ErrSessionIDEmpty   = errors.New("会话ID不能为空")
 	ErrPermissionDenied = errors.New("权限不足")
-	ErrTokenEmpty       = errors.New("token不能为空")
-	ErrTokenInvalid     = errors.New("token无效")
 )
